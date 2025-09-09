6e21fb02b39cc3a3ebb4eda9d027ed1d1101afba
Sajith Jeewantha <112331315+sajith-jeewantha@users.noreply.github.com>
2025-09-09 22:44:32 +0530

    Type

---
+++ app3.py
@@ -0,0 +1,11 @@
+x = input("x: ")
+print(type(x))  # <class 'str'>
+y = int(x) + 1
+print(y)  # 3
+
+#y = x + 1 #TypeError: can only concatenate str (not "int") to str
+
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
