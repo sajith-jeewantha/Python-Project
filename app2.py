493a30d7e95f0a92278533b683c989100b90e77b
Sajith Jeewantha <112331315+sajith-jeewantha@users.noreply.github.com>
2025-09-09 22:43:13 +0530

    Number

---
+++ app2.py
@@ -0,0 +1,15 @@
+import math
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+print(round(3.14))  # 3
+print(round(3.7))  # 4
+print(abs(-2.9))  # 2.9
+
+print(math.ceil(2.2))  # 3
