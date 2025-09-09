4989a21ee16aed23fb0805772a0676358e3868a2
Sajith Jeewantha <112331315+sajith-jeewantha@users.noreply.github.com>
2025-09-09 23:02:46 +0530

    Conditional

---
+++ app4.py
@@ -0,0 +1,19 @@
+temperature = 10
+
+if temperature > 30:
+    print("Hot")
+
+elif temperature > 15:
+    print("Normal")
+
+else:
+    print("Cold")
+
+print("Done")
+
+######
+
+age = 10
+
+message ="Eligible" if age >= 18 else "Not Eligible"
+print(message)
